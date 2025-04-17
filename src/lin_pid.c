a7956b63197e33d6878399e424b5cfeebb8a691a
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-04-17 03:07:44 -0500

    :building_construction: WIP: Home-brew'd parsing of user input
    Not yet working with even the most basic input.

--- src/lin_pid.c
+++ src/lin_pid.c
@@ -12,10 +12,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "lin_pid.h"
 
 /* Local Macro Definitions */
+#define MAX_INPUT_CHARS    10
+
 #define GET_BIT(x, n) ((x >> n) & 0x01)
+
+#ifdef TEST
+   #define STATIC // Set to nothing
+#else
+   #define STATIC static
+#endif
 
 /* Datatypes */
 
@@ -47,7 +57,9 @@ static const uint8_t SORTED_REFERENCE_PID_TABLE[MAX_ID_ALLOWED + 1] =
 
 /* Private Function Prototypes */
 
-int uint8_cmp( const void * a, const void * b );
+STATIC int UInt8_Cmp( const void * a, const void * b );
+STATIC bool GetID( char * str, uint8_t * id );
+STATIC bool MyAtoI(char digit, uint8_t * converted_digit);
 
 /* Meat of the Program */
 
@@ -59,16 +71,21 @@ int main(int argc, char * argv[])
 {
    /* Local variables */
    uint8_t pid = 0xFFu;
-   unsigned int user_input;
+   uint8_t user_input;
 
    /* Get user input */
    // printf("Input ID: ");
    // TODO: Use an alternative to scanf()
    // TODO: What about negative input?
    // TODO: What about non-hexadecimal digit inputs?
-   if ( argc >= 2 )
+   if ( (argc >= 2) || (argv[1] != NULL) )
    {
-      sscanf(argv[1], "%X", &user_input);
+      bool digits_read_successfully = GetID(argv[1], &user_input);
+      if ( !digits_read_successfully )
+      {
+         fprintf(stderr, "Invalid user input.");
+         return EXIT_FAILURE;
+      }
    }
    else
    {
@@ -96,7 +113,7 @@ int main(int argc, char * argv[])
                      SORTED_REFERENCE_PID_TABLE,
                      sizeof(SORTED_REFERENCE_PID_TABLE)/sizeof(uint8_t),
                      sizeof(uint8_t),
-                     uint8_cmp ) != NULL) );
+                     UInt8_Cmp ) != NULL) );
 
    /* Print Output */
    printf( "ID:  0x%02X\n", user_input );
@@ -141,7 +158,243 @@ uint8_t ComputePID(uint8_t id)
    return pid;
 }
 
-int uint8_cmp( const void * a, const void * b )
+// Acceptable formats: 0xZZ, ZZ, ZZh, xZZ, XZZ, ZZd
+//                     All will be assumed to be hexadecimal except ZZd.
+STATIC bool GetID( char * str, uint8_t * id )
+{
+   enum ParserState_E
+   {
+      ParserInit,
+      ParserOneZeroAlreadyDetected,
+      ParserIndeterminate,
+      ParserHexPrefix,
+      ParserIndeterminateDigits,
+      ParserDecDigits,
+      ParserHexDigits,
+      ParserHexSuffix,
+      ParserDecSuffix,
+      ParserTwoZerosAlreadyDetected,
+      ParserTwoDigitsAlreadyRead
+   } parser_state = ParserInit;
+
+   // Check input validity
+   if ( (NULL == str) || (NULL == id) )
+   {
+      return false;
+   }
+
+   uint8_t most_significant_digit = 0xFF;
+   uint8_t least_significant_digit = 0xFF;
+   bool ret_val = false;
+   size_t idx = 0;
+   size_t loop_limit_counter = 0;
+
+   // TODO: Do I need to check for this? Can I trust that the terminal will not
+   //       pass in white-space leading characters?
+   // Skip over any leading whitespace
+   while ( (loop_limit_counter <= MAX_INPUT_CHARS) &&
+           (str[idx] != '0') &&
+           (isblank( (int)str[idx] )) )
+   {
+      idx++;
+      loop_limit_counter++;
+   }
+   if ( str[idx] == '0' )
+   {
+      return false;
+   }
+
+   // Parser State Machine Time!
+   // First non-whitespace character should be either a a '0' or a hexadecimal digit
+   loop_limit_counter = 0;
+   bool exit_loop = false;
+   while ( (loop_limit_counter <= MAX_INPUT_CHARS) &&
+           (str[idx] != '\0') )
+   {
+      char ch = str[idx];
+
+      switch (parser_state)
+      {
+         case ParserInit:
+            if ( '0' == ch )
+            {
+               parser_state = ParserOneZeroAlreadyDetected;
+            }
+            else if ( isxdigit(ch) )
+            {
+               if ( isdigit(ch) )
+               {
+                  parser_state = ParserIndeterminateDigits;
+               }
+               else
+               {
+                  parser_state = ParserHexDigits;
+               }
+               bool atoi_conversion_successful = MyAtoI(ch, &most_significant_digit);
+               // The conversion should have been successful at this point
+               assert(atoi_conversion_successful == true);
+            }
+            else if ( ('x' == ch) && ('X' == ch) )
+            {
+               parser_state = ParserHexPrefix;
+            }
+            else
+            {
+               // Invalid character detected
+               // TODO: Throw exception - invalid character
+               exit_loop = true;
+            }
+            break;
+
+         case ParserOneZeroAlreadyDetected:
+            if ( '0' == ch )
+            {
+               parser_state = ParserTwoZerosAlreadyDetected;
+            }
+            else if ( ('x' == ch) || ('X' == ch) )
+            {
+               parser_state = ParserHexPrefix;
+            }
+            break;
+
+         case ParserIndeterminate:
+            break;
+
+
+         case ParserHexPrefix:
+            if ( isxdigit(ch) )
+            {
+               bool atoi_conversion_successful = MyAtoI(ch, &most_significant_digit);
+               // The conversion should have been successful at this point
+               assert(atoi_conversion_successful == true);
+
+               parser_state = ParserHexDigits;
+            }
+            else
+            {
+               // Invalid character detected
+               // TODO: Throw exception - invalid character
+               exit_loop = true;
+            }
+            break;
+
+         case ParserIndeterminateDigits:
+            break;
+
+         case ParserDecDigits:
+            break;
+         
+         case ParserHexDigits:
+            if ( isxdigit(ch) )
+            {
+               bool atoi_conversion_successful = MyAtoI(ch, &least_significant_digit);
+               // The conversion should have been successful at this point
+               assert(atoi_conversion_successful == true);
+
+               parser_state = ParserTwoDigitsAlreadyRead;
+            }
+            else
+            {
+               // Invalid character detected
+               // TODO: Throw exception - invalid character
+               exit_loop = true;
+            }
+            break;
+
+         case ParserHexSuffix:
+            break;
+
+         case ParserDecSuffix:
+            break;
+
+         case ParserTwoZerosAlreadyDetected:
+            if ( '0' == ch )
+            {
+               // Too many 0 digits in a row. Invalid input!
+               // TODO: Throw exception - too many 0s in a row.
+               exit_loop = true;
+            }
+            break;
+
+         case ParserTwoDigitsAlreadyRead:
+            // Too many digits in a row. Invalid input!
+            // TODO: Throw exception - too many digits passed in.
+            exit_loop = true;
+            break;
+
+         default:
+            break;
+      }
+      
+      if ( exit_loop )
+      {
+         break;
+      }
+
+      idx++;
+      loop_limit_counter++;
+   }
+   
+   if ( exit_loop )
+   {
+      // Invalid digits were detected! Don't update ID.
+   }
+   else
+   {
+      // Update ID
+      // Digits obtained should be <= 0xF
+      assert( (most_significant_digit <= 0x0F) && (least_significant_digit <= 0x0F) );
+      *id = (uint8_t)( (most_significant_digit * 10) + least_significant_digit );
+
+      ret_val = true;
+   }
+   
+   if ( loop_limit_counter >= MAX_INPUT_CHARS )
+   {
+      // TODO: Throw exception because we should have stopped sooner than that!
+      // This means the string was not null-terminated.
+   }
+
+   return ret_val;
+}
+
+STATIC bool MyAtoI(char digit, uint8_t * converted_digit)
+{
+   bool ret_val = false;
+
+   // Check input validity
+   if ( ( converted_digit != NULL ) ||
+          ( (digit >= 'A') && (digit <= 'F') ) ||
+          ( (digit >= 'a') && (digit <= 'f') )
+      )
+   {
+      if ( (digit >= '0') && (digit <= '9') )
+      {
+         *converted_digit = (uint8_t)(digit - '0');
+         ret_val = true;
+      }
+      else if ( (digit >= 'A') && (digit <= 'F') )
+      {
+         *converted_digit = (uint8_t)( 10 + (digit - 'A') );
+         ret_val = true;
+      }
+      else if ( (digit >= 'a') && (digit <= 'f') )
+      {
+         *converted_digit = (uint8_t)( 10 + (digit - 'a') );
+         ret_val = true;
+      }
+      else
+      {
+         // Invalid digit
+         // TODO: Throw exception - invalid digit
+         // Leave ret_val to its initialized value
+      }
+   }
+
+   return ret_val;
+}
+
+int UInt8_Cmp( const void * a, const void * b )
 {
    uint8_t * c = (uint8_t *)a;
    uint8_t * d = (uint8_t *)b;
