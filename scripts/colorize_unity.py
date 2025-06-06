45e436f806ee8e9ff4fca60bfecf13ec49d2057e
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-06-06 07:20:25 -0500

    :pick: :paintbrush: `colorize_unity.py`: More coloring for ease of reading

--- scripts/colorize_unity.py
+++ scripts/colorize_unity.py
@@ -2,15 +2,16 @@ import sys
 import re
 
 # Define colors
-GREEN = "\033[1;32m"
 RED = "\033[1;31m"
+GREEN = "\033[1;32m"
 YEL = "\033[0;33m"
 BLU = "\033[1;34m"
+MAGENTA = "\033[1;35m"
 CYAN = "\033[1;36m"
 RESET = "\033[0m"
 
 # Combined regex to match severity levels
-test_result_pattern = re.compile(r"(PASS)|(FAIL)|(IGNORE)|(INFO)")
+test_result_pattern = re.compile(r"(PASS)|(FAIL)|(IGNORE)|([\d]+:)|(^test/[\w_\-\.]+:)")
 
 def colorize(match):
     if match.group(1):  # Matches "PASS"
@@ -19,12 +20,14 @@ def colorize(match):
         return f"{RED}{match.group(2)}{RESET}"
     elif match.group(3):  # Matches "IGNORE"
         return f"{BLU}{match.group(3)}{RESET}"
-    elif match.group(4):  # Matches "INFO"
+    elif match.group(4):  # Matches "Line number"
         return f"{CYAN}{match.group(4)}{RESET}"
+    elif match.group(5):  # Matches "Filename"
+        return f"{MAGENTA}{match.group(5)}{RESET}"
 
 # Check for input
 if sys.stdin.isatty() and sys.stderr.isatty():
-    print("No input provided. Please pipe cppcheck output into this script.", file=sys.stderr)
+    print("No input provided. Please pipe Unity output into this script.", file=sys.stderr)
     sys.exit(1)
 
 # Process input line by line from stderr
